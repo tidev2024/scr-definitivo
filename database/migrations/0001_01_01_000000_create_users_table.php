9be59eb940d4b73c4998442493ae1774cd3c2bbf
lucaFerreira <lucaferreira@romagroup.com.br>
2024-05-24 14:44:18 -0300

    removendo coluna de user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->boolean('active');
             $table->boolean('master');
-            $table->rememberToken();
             $table->timestamps();
         });
 
