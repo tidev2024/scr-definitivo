e43395aa5a77da4d259c018970a791b35ab2a6fb
Julio T.I <juliooliveira@romagroup.com.br>
2024-06-05 09:26:53 -0300

    Create e Edit user alterações

--- resources/views/user/create-user.blade.php
+++ resources/views/user/create-user.blade.php
@@ -1,67 +1,134 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <form action="{{ route('user.store') }}" method="post">
-        @csrf
-        <input type="text" name="name" id="name" placeholder="name" value="{{ old('name') }}">
-        @error('name')
-            {{ $message }}
-        @enderror
-        <input type="text" name="cpf" id="cpf" placeholder="cpf" value="{{ old('cpf') }}">
-        @error('cpf')
-            {{ $message }}
-        @enderror
-        <input type="password" name="password" id="password" placeholder="password" >
-        @error('password')
-            {{ $message }}
-        @enderror
-        <input type="text" name="email" id="email" placeholder="email" value="{{ old('email') }}">
-        @error('email')
-            {{ $message }}
-        @enderror
-        <input type="checkbox" name="active" id="active" placeholder="active" value="1" {{ old('active') ? 'checked' : '' }}>
-        <input type="checkbox" name="master" id="master" placeholder="master" value="1" {{ old('master') ? 'checked' : '' }}>
-        <select name="company_id" id="company_id">
-            <option value="">Seleciona uma empresa</option>
-            @foreach ($companies as $company)
-                <option value="{{ $company->id }}" {{ old('company_id') == $company->id ? 'selected' : '' }}>{{ $company->name }}</option>
-            @endforeach
-        </select>
-        @error('company_id')
-            {{ $message }}
-        @enderror
-        <select name="department_id" id="department_id">
-            <option value="">Seleciona um departamento</option>
-            @foreach ($departments as $department)
-                <option value="{{ $department->id }}" {{ old('department_id') == $department->id ? 'selected' : '' }}>{{ $department->name }}</option>
-            @endforeach
-        </select>
-        @error('department_id')
-            {{ $message }}
-        @enderror
-        <select name="position_id" id="position_id">
-            <option value="">Seleciona um cargo</option>
-            @foreach ($positions as $position)
-                <option value="{{ $position->id }}" {{ old('position_id') == $position->id ? 'selected' : '' }}>{{ $position->name }}</option>
-            @endforeach
-        </select>
-        @error('position_id')
-            {{ $message }}
-        @enderror
-        @foreach ($permissions as $menu => $permissionValues)
-            <h2>{{ $menu }}</h2>
-            @foreach ($permissionValues as $id => $value)
-                {{ $value }}
-                <input type="checkbox" name="permission_id[]" id="" value="{{ $id }}">
-            @endforeach
-        @endforeach
-        <button type="submit">Enviar</button>
-    </form>
-</body>
-</html>
+@extends('default.layout')
+
+@section('content')
+
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Cadastrar Usuário</h4>
+                </div>
+                <div class="card-body">
+                    <form action="{{ route('user.store') }}" method="post">
+                        @csrf
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                <label for="name">Nome</label>
+                                <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name" placeholder="" value="{{ old('name') }}">
+                                @error('name')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+
+                            <div class="form-group col-md-6">
+                                <label for="cpf">CPF</label>
+                                <input type="text" class="form-control @error('cpf') is-invalid @enderror" name="cpf" id="cpf" placeholder="CPF" value="{{ old('cpf') }}">
+                                @error('cpf')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                <label for="password">Senha</label>
+                                <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password" placeholder="">
+                                @error('password')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+
+                            <div class="form-group col-md-6">
+                                <label for="email">Email</label>
+                                <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" placeholder="Email" value="{{ old('email') }}">
+                                @error('email')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="row">
+                        <div class="form-group col-md-6">
+                            <input type="checkbox" class="form-check-input" name="active" id="active" value="1" {{ old('active') ? 'checked' : '' }}>
+                            <label class="form-check-label" for="active">Ativo</label>
+                        </div>
+
+                        <div class="form-group col-md-6">
+                            <input type="checkbox" class="form-check-input" name="master" id="master" value="1" {{ old('master') ? 'checked' : '' }}>
+                            <label class="form-check-label" for="master">Master</label>
+                        </div>
+                      </div>
+  
+                        <div class="form-group mb-3">
+                            <label for="company_id">Company</label>
+                            <select class="form-control @error('company_id') is-invalid @enderror" name="company_id" id="company_id">
+                                <option value="">Selecione uma empresa</option>
+                                @foreach ($companies as $company)
+                                    <option value="{{ $company->id }}" {{ old('company_id') == $company->id ? 'selected' : '' }}>{{ $company->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('company_id')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="form-group mb-3">
+                            <label for="department_id">Department</label>
+                            <select class="form-control @error('department_id') is-invalid @enderror" name="department_id" id="department_id">
+                                <option value="">Selecione um departamento</option>
+                                @foreach ($departments as $department)
+                                    <option value="{{ $department->id }}" {{ old('department_id') == $department->id ? 'selected' : '' }}>{{ $department->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('department_id')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="form-group mb-3">
+                            <label for="position_id">Position</label>
+                            <select class="form-control @error('position_id') is-invalid @enderror" name="position_id" id="position_id">
+                                <option value="">Posição</option>
+                                @foreach ($positions as $position)
+                                    <option value="{{ $position->id }}" {{ old('position_id') == $position->id ? 'selected' : '' }}>{{ $position->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('position_id')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <center><h2>Permissões</h2></center>
+
+                        @foreach ($permissions as $menu => $permissionValues)
+                            <h5>{{ $menu }}</h5>
+                            @foreach ($permissionValues as $id => $value)
+                                <div class="form-check">
+                                    <input type="checkbox" class="form-check-input" name="permission_id[]" value="{{ $id }}">
+                                    <label class="form-check-label">{{ $value }}</label>
+                                </div>
+                            @endforeach
+                        @endforeach
+
+                        <button type="submit" class="btn btn-primary mt-3">Salvar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
--- resources/views/user/edit-user.blade.php
+++ resources/views/user/edit-user.blade.php
@@ -1,64 +1,121 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <form action="{{ route('user.update', $user->id) }}" method="post">
-        @csrf
-        @method('put')
-        <input type="hidden" name="id" value="{{ $user->id }}">
-        <input type="text" name="name" id="name" placeholder="name" value="{{ $user->name }}">
-        @error('name')
-            {{ $message }}
-        @enderror
-        <input type="text" name="cpf" id="cpf" placeholder="cpf" value="{{ $user->cpf }}">
-        @error('cpf')
-            {{ $message }}
-        @enderror
-        <input type="text" name="email" id="email" placeholder="email" value="{{ $user->email }}">
-        @error('email')
-            {{ $message }}
-        @enderror
-        <input type="checkbox" name="active" id="active" placeholder="active" value="1" {{ $user->active ? 'checked' : '' }}>
-        @error('active')
-            {{ $message }}
-        @enderror
-        <input type="checkbox" name="master" id="master" placeholder="master" value="1" {{ $user->master ? 'checked' : '' }}>
-        @error('master')
-            {{ $message }}
-        @enderror
-        <select name="company_id" id="company_id">
-            <option value="">Seleciona uma empresa</option>
-            @foreach ($companies as $company)
-                <option value="{{ $company->id }}" {{ $user->company_id == $company->id ? 'selected' : '' }}>{{ $company->name }}</option>
-            @endforeach
-        </select>
-        @error('company_id')
-            {{ $message }}
-        @enderror
-        <select name="department_id" id="department_id">
-            <option value="">Seleciona um departamento</option>
-            @foreach ($departments as $department)
-                <option value="{{ $department->id }}" {{ $user->department_id == $department->id ? 'selected' : '' }}>{{ $department->name }}</option>
-            @endforeach
-        </select>
-        @error('department_id')
-            {{ $message }}
-        @enderror
-        <select name="position_id" id="position_id">
-            <option value="">Seleciona um cargo</option>
-            @foreach ($positions as $position)
-                <option value="{{ $position->id }}" {{ $user->position_id == $position->id ? 'selected' : '' }}>{{ $position->name }}</option>
-            @endforeach
-        </select>
-        @error('position_id')
-            {{ $message }}
-        @enderror
-        <button type="submit">Enviar</button>
-    </form>
-</body>
-</html>
+@extends('default.layout')
+
+@section('content')
+
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Edit User</h4>
+                </div>
+                <div class="card-body">
+                    <form action="{{ route('user.update', $user->id) }}" method="post">
+                        @csrf
+                        @method('put')
+                        <input type="hidden" name="id" value="{{ $user->id }}">
+
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                            <label for="name">Name</label>
+                            <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name" placeholder="Name" value="{{ $user->name }}">
+                            @error('name')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                            </div>
+                
+                         <div class="form-group col-md-6">
+                            <label for="cpf">CPF</label>
+                            <input type="text" class="form-control @error('cpf') is-invalid @enderror" name="cpf" id="cpf" placeholder="CPF" value="{{ $user->cpf }}">
+                            @error('cpf')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" placeholder="Email" value="{{ $user->email }}">
+                            @error('email')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <input type="checkbox" class="form-check-input" name="active" id="active" value="1" {{ $user->active ? 'checked' : '' }}>
+                            <label class="form-check-label" for="active">Active</label>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <input type="checkbox" class="form-check-input" name="master" id="master" value="1" {{ $user->master ? 'checked' : '' }}>
+                            <label class="form-check-label" for="master">Master</label>
+                        </div>
+                    </div>
+                        <div class="form-group">
+                            <label for="company_id">Company</label>
+                            <select class="form-control @error('company_id') is-invalid @enderror" name="company_id" id="company_id">
+                                <option value="">Select a company</option>
+                                @foreach ($companies as $company)
+                                    <option value="{{ $company->id }}" {{ $user->company_id == $company->id ? 'selected' : '' }}>{{ $company->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('company_id')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="department_id">Department</label>
+                            <select class="form-control @error('department_id') is-invalid @enderror" name="department_id" id="department_id">
+                                <option value="">Select a department</option>
+                                @foreach ($departments as $department)
+                                    <option value="{{ $department->id }}" {{ $user->department_id == $department->id ? 'selected' : '' }}>{{ $department->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('department_id')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="position_id">Position</label>
+                            <select class="form-control @error('position_id') is-invalid @enderror" name="position_id" id="position_id">
+                                <option value="">Select a position</option>
+                                @foreach ($positions as $position)
+                                    <option value="{{ $position->id }}" {{ $user->position_id == $position->id ? 'selected' : '' }}>{{ $position->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('position_id')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <h2>Permissions</h2>
+                            @foreach ($permissions as $menu => $permissionValues)
+                                <h5>{{ $menu }}</h5>
+                                @foreach ($permissionValues as $id => $value)
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" name="permission_id[]" value="{{ $id }}" {{ $user->permissions->contains('id', $id) ? 'checked' : '' }}>
+                                        <label class="form-check-label">{{ $value }}</label>
+                                    </div>
+                                @endforeach
+                            @endforeach
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
